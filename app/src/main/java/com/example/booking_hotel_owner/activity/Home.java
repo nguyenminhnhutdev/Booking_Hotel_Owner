2223ddb14337d1af22ab6d1296bf9b1a33800666
Hai Dang <lehaidangxh@gmail.com>
2022-01-08 14:56:11 +0700

    cxc

--- app/src/main/java/com/example/booking_hotel_owner/activity/Home.java
+++ app/src/main/java/com/example/booking_hotel_owner/activity/Home.java
@@ -1,17 +1,58 @@
 package com.example.booking_hotel_owner.activity;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.example.booking_hotel_owner.Capture;
 import com.example.booking_hotel_owner.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class Home extends AppCompatActivity {
-
+ImageView Scan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
+        Scan=findViewById(R.id.Scan);
+        Scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator intentIntegrator= new IntentIntegrator(Home.this);
+                intentIntegrator.setPrompt("fff");
+                intentIntegrator.setBeepEnabled(true);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setCaptureActivity(Capture.class);
+                intentIntegrator.initiateScan();
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult= IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(intentResult.getContents()!=null)
+        {
+            AlertDialog.Builder builder= new AlertDialog.Builder(Home.this);
+            builder.setMessage(intentResult.getContents());
+            builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
+            builder.show();
+        }else {
+            Toast.makeText(Home.this, "k thjeer scan", Toast.LENGTH_SHORT).show();
+        }
     }
 }
