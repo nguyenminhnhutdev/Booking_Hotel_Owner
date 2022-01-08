0ef84af854254a4905cf57ba48f4bb2a5e2efc12
Thuận Trần <thuantran190200@gmail.com>
2022-01-08 22:33:56 +0700

    Thuan_Design

--- app/src/main/java/com/example/booking_hotel_owner/fragment/home.java
+++ app/src/main/java/com/example/booking_hotel_owner/fragment/home.java
@@ -1,5 +1,6 @@
 package com.example.booking_hotel_owner.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,18 +8,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.booking_hotel_owner.R;
+import com.example.booking_hotel_owner.activity.ThemKhachSanPhong;
 
 
 public class home extends Fragment {
 
 
+    ImageView btn_add;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        btn_add = (ImageView) view.findViewById(R.id.btn_add);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), ThemKhachSanPhong.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
