c733356c126b5bd10a6f059c2ff9e7946513d96d
Hai Dang <lehaidangxh@gmail.com>
2022-01-08 14:56:11 +0700

    cxc

---
+++ app/src/main/java/com/example/booking_hotel_owner/Capture.java
@@ -0,0 +1,8 @@
+package com.example.booking_hotel_owner;
+
+import android.app.Activity;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class Capture extends CaptureActivity {
+}
