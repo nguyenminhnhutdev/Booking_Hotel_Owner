dc73c03f49aa02516ce0ecf4f0f38fcb7bee955d
Thuận Trần <thuantran190200@gmail.com>
2022-01-09 14:06:37 +0700

    Thuan_Design

--- app/src/main/java/com/example/booking_hotel_owner/activity/ThemKhachSanPhong.java
+++ app/src/main/java/com/example/booking_hotel_owner/activity/ThemKhachSanPhong.java
@@ -5,20 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.booking_hotel_owner.R;
 
 public class ThemKhachSanPhong extends AppCompatActivity {
 
     ImageView btn_back;
+    TextView btn_thoat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_khach_san_phong);
 
+        btn_thoat = findViewById(R.id.btn_thoat);
         btn_back = findViewById(R.id.btn_back);
 
         btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        btn_thoat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
